4db65672073083aac60a9511dc81630362c0b5ad
ROn <geniusron@gmail.com>
2026-05-11 13:55:44 +0800

    feat: add packet tunnel loop runner

---
+++ apps/irock-iOS/irockTunnelExtension/IOSPacketTunnelLoopRunner.swift
@@ -0,0 +1,22 @@
+import Foundation
+import NetworkExtension
+
+struct IOSPacketTunnelLoopRunner: Sendable {
+    private let smokeRunner: IOSPacketTunnelSmokeRunner
+    private let loopDelayNanoseconds: UInt64
+
+    init(
+        smokeRunner: IOSPacketTunnelSmokeRunner = IOSPacketTunnelSmokeRunner(),
+        loopDelayNanoseconds: UInt64 = 50_000_000
+    ) {
+        self.smokeRunner = smokeRunner
+        self.loopDelayNanoseconds = loopDelayNanoseconds
+    }
+
+    func run(packetFlow: NEPacketTunnelFlow) async throws {
+        while !Task.isCancelled {
+            _ = try await smokeRunner.runOnce(packetFlow: packetFlow)
+            try await Task.sleep(nanoseconds: loopDelayNanoseconds)
+        }
+    }
+}
